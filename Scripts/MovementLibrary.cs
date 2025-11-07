498d590a91e0ecec0d52b1db90c2243acb969123
AnnickS <annick.stefanin@gmail.com>
2025-11-06 18:14:07 -0600

    Made methods for movement library

--- Scripts/MovementLibrary.cs
+++ Scripts/MovementLibrary.cs
@@ -3,4 +3,25 @@ using System;
 
 public partial class MovementLibrary : GodotObject
 {
+
+    public Vector2 MoveInStraightLine(Vector2 Mover, Vector2 Target)
+    {
+        Vector2 MoveUnitVector = new Vector2((Target.X - Mover.X), (Target.Y - Mover.Y));
+
+        return MoveUnitVector;
+    }
+
+    public Vector2 MoveClockwiseToObject( Vector2 Target)
+    {
+        Vector2 MoveVector = new Vector2();
+
+        return MoveVector;
+    }
+    
+    public Vector2 MoveCounterClockwiseToObject( Vector2 Target)
+    {
+        Vector2 MoveVector = new Vector2();
+
+        return MoveVector;
+    }
 }
