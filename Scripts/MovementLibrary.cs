5e9a38a291a10a2ef83e5ae187a749de90b1c8d8
AnnickS <annick.stefanin@gmail.com>
2025-11-06 18:35:55 -0600

    Added Move Function

--- Scripts/MovementLibrary.cs
+++ Scripts/MovementLibrary.cs
@@ -6,20 +6,25 @@ public partial class MovementLibrary : GodotObject
 
     public Vector2 MoveInStraightLine(Vector2 Mover, Vector2 Target)
     {
-        Vector2 MoveUnitVector = new Vector2((Target.X - Mover.X), (Target.Y - Mover.Y));
-
+        Vector2 MoveUnitVector = Mover.DirectionTo(Target);
         return MoveUnitVector;
     }
 
-    public Vector2 MoveClockwiseToObject( Vector2 Target)
+    public Vector2 OrbitClockwiseAroundObject( Vector2 Target)
     {
+        /*
+        TODO: Implement
+        */
         Vector2 MoveVector = new Vector2();
 
         return MoveVector;
     }
     
-    public Vector2 MoveCounterClockwiseToObject( Vector2 Target)
+    public Vector2 OrbitCounterClockwiseAroundObject( Vector2 Target)
     {
+        /*
+        TODO: Implement
+        */
         Vector2 MoveVector = new Vector2();
 
         return MoveVector;
