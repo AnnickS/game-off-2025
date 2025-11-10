f11e42c32dd9d9d19fca725efe867c0711e84528
JasonLovrien <54682995+JasonLovrien@users.noreply.github.com>
2025-11-09 22:09:47 -0600

    Add Autoload Script to Project and Update Audload to EnterTree
    The Autoload script wasn't loading because it wasn't added in the godot project. Also, when using _Ready() the GlobalAutoload wouldn't load before the Hero EnterTree() function was called because _Ready() happens later. Updated Autoload script to use EnterTree() and now it works.

--- Scripts/GlobalAutoload.cs
+++ Scripts/GlobalAutoload.cs
@@ -5,11 +5,10 @@ public partial class GlobalAutoload : Node
 {
     public static GlobalAutoload Instance { get; private set; }
 
-    public Node Hero { get; set; }
+    public Node2D Hero { get; set; }
 
-    public override void _Ready()
+    public override void _EnterTree()
     {
         Instance = this;
     }
-
 }
