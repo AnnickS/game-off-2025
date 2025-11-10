2d224da60ebbd8522424f165c20c6896118750ef
JasonLovrien <54682995+JasonLovrien@users.noreply.github.com>
2025-11-10 17:53:17 -0600

    Add Movement to Goon
    Goon now moves towards the Hero at a constant speed.

--- Scenes/Goon/Goon.cs
+++ Scenes/Goon/Goon.cs
@@ -3,4 +3,12 @@ using System;
 
 public partial class Goon : Node2D
 {
+    private float Speed = 400F;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+        Position += Position.DirectionTo(GlobalAutoload.Instance.Hero.Position) * Speed * (float)delta;
+    }
+
 }
