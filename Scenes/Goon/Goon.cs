da5cf80a2bfe1cfaddc39c2512f248ba921f499a
JasonLovrien <54682995+JasonLovrien@users.noreply.github.com>
2025-11-10 21:08:43 -0600

    #5 Update Goon to Die After Attacking
    Thought it might make sense to have some rusher that just run into the hero and deal damage when they collide with them

--- Scenes/Goon/Goon.cs
+++ Scenes/Goon/Goon.cs
@@ -3,7 +3,23 @@ using System;
 
 public partial class Goon : Node2D
 {
-    private float Speed = 400F;
+    [Export]
+    private float Speed = 400f;
+
+    [Export]
+    private AttackComponent Attack;
+
+    public override void _Ready()
+    {
+        base._Ready();
+        Attack.HitHurtbox += QueueFree;
+    }
+
+    public override void _ExitTree()
+    {
+        base._ExitTree();
+        Attack.HitHurtbox -= QueueFree;
+    }
 
     public override void _PhysicsProcess(double delta)
     {
--- Scenes/Shared_Components/AttackComponent.cs
+++ Scenes/Shared_Components/AttackComponent.cs
@@ -6,6 +6,9 @@ public partial class AttackComponent : Area2D
     [Export]
     public int damage;
 
+    [Signal]
+    public delegate void HitHurtboxEventHandler();
+
     public override void _Ready()
     {
         base._Ready();
@@ -23,6 +26,7 @@ public partial class AttackComponent : Area2D
         if(areaEntered is HurtBox)
         {
             ((HurtBox)areaEntered).ReceiveAttack(damage);
+            EmitSignal(SignalName.HitHurtbox);
         }
     }
 }
