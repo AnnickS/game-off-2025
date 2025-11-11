52d183b7027fae2283754915da1c30d3ed9849a6
JasonLovrien <54682995+JasonLovrien@users.noreply.github.com>
2025-11-11 15:18:39 -0600

    #6 Add Navigation Agent to Hero And Update Goon and Hero to CharacterBody2D
    Added navigation agent to hero but it wasn't avoiding. So I added some collision shapes (ended up being unnecessary) and figured I'd make them CharacterBody2D because that made sense for what they would be.
    
    So, right now "Avoid" is on for the Goon AND hero. However, the Goons don't actually calculate the safe velocity, so they still just go straight. It might be screwing with the pathing of the Hero. Will have to do some masking stuff so that the Hero avoids Goons but the Goons don't avoid HEro.

--- Scenes/Goon/Goon.cs
+++ Scenes/Goon/Goon.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class Goon : Node2D
+public partial class Goon : CharacterBody2D
 {
     [Export]
     private float Speed = 400f;
--- Scenes/Hero/Hero.cs
+++ Scenes/Hero/Hero.cs
@@ -1,10 +1,13 @@
 using Godot;
 using System;
 
-public partial class Hero : Node2D
+public partial class Hero : CharacterBody2D
 {
     [Export]
     HealthComponent Health;
+
+    [Export]
+    NavigationAgent2D NavigationAgent;
 
     public override void _EnterTree()
     {
@@ -12,15 +15,44 @@ public partial class Hero : Node2D
         GlobalAutoload.Instance.Hero = this;
     }
 
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+
+        if (NavigationAgent.IsNavigationFinished())
+        {
+            return;
+        }
+
+
+        Vector2 PlannedVelocity = ToLocal(NavigationAgent.GetNextPathPosition()).Normalized() * 400f;
+        NavigationAgent.Velocity = PlannedVelocity;
+    }
+    
+    private void OnVelocityComputed(Vector2 safeVelocity)
+    {
+        Velocity = safeVelocity;
+        MoveAndSlide();
+    }
+
+
     public override void _Ready()
     {
         base._Ready();
         Health.HealthDepleted += Die;
+        CallDeferred("SetupMovement");
+    }
+
+    private void SetupMovement()
+    {
+        NavigationAgent.TargetPosition = GlobalPosition + GlobalPosition;
+        NavigationAgent.VelocityComputed += OnVelocityComputed;
     }
 
     public override void _ExitTree()
     {
         Health.HealthDepleted -= Die;
+        NavigationAgent.VelocityComputed -= OnVelocityComputed;
         base._ExitTree();
     }
 
