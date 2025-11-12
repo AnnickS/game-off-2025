c65a7f467c814bebc2d490072d37dfa5914f2936
JasonLovrien <54682995+JasonLovrien@users.noreply.github.com>
2025-11-11 18:39:36 -0600

    #7 Add action listener in Level
    Level now listens for mouse clicks and spawns a Goon wherever the click is.

---
+++ Scenes/Level/Level.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class Level : Node2D
+{
+    PackedScene PackedGoon = GD.Load<PackedScene>("res://Scenes/Goon/Goon.tscn");
+    public override void _UnhandledInput(InputEvent @event)
+    {
+        base._UnhandledInput(@event);
+
+        if (@event is InputEventMouseButton eventMouseButton && !eventMouseButton.IsReleased())
+        {
+            Node2D NewGoon = PackedGoon.Instantiate<Node2D>();
+            NewGoon.Position = eventMouseButton.Position;
+            AddChild(NewGoon);
+        }
+    }
+
+}
